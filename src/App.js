b1675d005ebfd5f8d7a7c3357f22c4d5e41a80d9
index00913 <jisu00913@gmail.com>
2023-09-26 15:27:02 +0900

    header 기본 스타일

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { Route } from 'react-router-dom';
 import Header from './components/common/header/Header';
 import Department from './components/sub/department/Department';
 import Youtube from './components/sub/youtube/Youtube';
+import Members from './components/sub/members/Members';
+import Contact from './components/sub/contact/Contact';
+import Gallery from './components/sub/gallery/Gallery';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Route path='/department'>
-				<Department />
-			</Route>
-			<Route path='/Youtube'>
-				<Youtube />
-			</Route>
+			<Route path='/department' component={Department} />
+			<Route path='/youtube' component={Youtube} />
+			<Route path='/members' component={Members} />
+			<Route path='/contact' component={Contact} />
+			<Route path='/gallery' component={Gallery} />
 		</>
 	);
 }
---
+++ src/components/sub/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import Layout from '../../common/layout/Layout';
+//import styles from './Department.module.scss';
+//import clsx from 'clsx';
+
+export default function Contact() {
+	return (
+		<Layout title={'Contact'}>
+			<p>컨텍트 페이지입니다.</p>
+		</Layout>
+	);
+}
