a974c5614ece5aa804196d1e2acd2810b9b6d028
index00913 <jisu00913@gmail.com>
2023-09-26 14:19:19 +0900

    모든 서브페이지에 적용된 공통 레이아웃 컴포넌트를 각각의 페이지 컴포넌트에 적용

---
+++ src/components/sub/department/Department.jsx
@@ -0,0 +1,11 @@
+import Layout from '../../common/layout/Layout';
+//import styles from './Department.module.scss';
+//import clsx from 'clsx';
+
+export default function Department() {
+	return (
+		<Layout>
+			<p>다파트먼트 페이지입니다.</p>
+		</Layout>
+	);
+}
