1271dc5e6c8825b81773b924f9c49891b5f5e67f
index00913 <jisu00913@gmail.com>
2023-10-04 10:26:47 +0900

    .

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,11 +1,20 @@
 import Layout from '../../common/layout/Layout';
 //import styles from './Department.module.scss';
 //import clsx from 'clsx';
+import styles from './Department.module.scss';
+
+console.log(styles);
 
 export default function Department() {
 	return (
 		<Layout title={'Department'}>
 			<p>다파트먼트 페이지입니다.</p>
+			<ul>
+				<li className={styles.abc}>list1</li>
+				<li>list2</li>
+				<li>list3</li>
+				<li>list4</li>
+			</ul>
 		</Layout>
 	);
 }
