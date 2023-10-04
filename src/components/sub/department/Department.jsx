e35d87b55186f0812b714d9ea0bbcf04fe3a70f3
index00913 <jisu00913@gmail.com>
2023-10-04 10:34:37 +0900

    useEffect로 캄포넌트 마운트시 한번만 외부 데이터 fetch

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -2,10 +2,19 @@ import Layout from '../../common/layout/Layout';
 //import styles from './Department.module.scss';
 //import clsx from 'clsx';
 import styles from './Department.module.scss';
+import { useEffect } from 'react';
 
 console.log(styles);
 
 export default function Department() {
+	useEffect(() => {
+		//해당 useEffect구문은 컴포넌트 마운트시 한번만 동작됨
+		fetch('/DB/department.json')
+			.then((data) => data.json())
+			.then((json) => {
+				console.log(json);
+			});
+	}, []);
 	return (
 		<Layout title={'Department'}>
 			<p>다파트먼트 페이지입니다.</p>
